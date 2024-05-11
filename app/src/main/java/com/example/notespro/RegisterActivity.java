4bbe598ad7c5c806ac4459f5fc1133f355ce4089
Dan <kromekdani@gmail.com>
2024-05-11 20:06:38 +0200

    Register with Firebase

--- app/src/main/java/com/example/notespro/RegisterActivity.java
+++ app/src/main/java/com/example/notespro/RegisterActivity.java
@@ -1,14 +1,33 @@
 package com.example.notespro;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText emailEditText,passwordEditText,confirmPasswordEditText;
+    Button registerButton;
+    ProgressBar progressBar;
+    TextView loginTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,79 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.conformed_password_edit_text);
+        registerButton = findViewById(R.id.create_account_button);
+        progressBar = findViewById(R.id.progress_bar);
+        loginTextView = findViewById(R.id.login_text_view_button);
+
+        registerButton.setOnClickListener( v -> register());
+        loginTextView.setOnClickListener( v -> finish());
+
+    }
+
+    void register(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email,password,confirmPassword);
+        if(!isValidated){
+            return;
+        }
+
+        createAccountInFirebase(email,password);
+    }
+
+    boolean validateData(String email, String password, String confirmPassword){
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Password is too short");
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            passwordEditText.setError("Passwords do not match");
+            return false;
+        }
+        return true;
+    }
+
+    void createAccountInFirebase(String email, String password){
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        changeInProgress(false);
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Account is successfully created", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+    }
+
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            registerButton.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            registerButton.setVisibility(View.VISIBLE);
+        }
     }
 }
